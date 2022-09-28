047590fb22f40e0293f82588f0187f91acabf908
Malcolm Barrett <malcolmbarrett@gmail.com>
2022-09-28 16:58:17 -0400

    set envir in alert

--- R/utils.R
+++ R/utils.R
@@ -2,8 +2,8 @@ be_quiet <- function() {
   getOption("propensity.quiet", default = FALSE)
 }
 
-alert_info <- function(.message) {
+alert_info <- function(.message, .envir = parent.frame()) {
   if (!be_quiet()) {
-    cli::cli_alert_info(text = .message)
+    cli::cli_alert_info(text = .message, .envir = .envir)
   }
 }
